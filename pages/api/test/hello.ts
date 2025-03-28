2951e1038a57e6cd91f3791eaac5ae41bad61b2c
MoraMateo <christopher.mora@educampeche.mx>
2025-03-28 16:12:04 -0600

    fix complete

--- pages/api/test/hello.ts
+++ pages/api/test/hello.ts
@@ -1,5 +1,5 @@
 import type { NextApiRequest, NextApiResponse } from "next";
 
 export default function handler(req: NextApiRequest, res: NextApiResponse) {
-  res.status(200).send("¡El endpoint SÍ está funcionando!");
+  res.status(200).send("Test OK desde /api/test/hello");
 }

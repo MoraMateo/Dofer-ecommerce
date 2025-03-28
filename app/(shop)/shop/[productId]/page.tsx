69e58d93711d8aa6475aec4dba90a1f08fd2880d
MoraMateo <christopher.mora@educampeche.mx>
2025-03-28 00:56:27 -0600

    refactor desing product detail cliente

--- app/(shop)/shop/[productId]/page.tsx
+++ app/(shop)/shop/[productId]/page.tsx
@@ -1,7 +1,6 @@
 import { getProductById } from "@/services/wooCommerce";
 import ProductDetailClient from "./ProductDetailClient";
 
-// Indica que esta ruta es completamente dinámica y desactiva el caché
 export const dynamic = "force-dynamic";
 export const revalidate = 0;
 
@@ -10,13 +9,6 @@ interface ProductDetailProps {
 }
 
 export default async function ProductDetailPage({ params }: ProductDetailProps) {
-  // Obtiene el producto según su ID
   const product = await getProductById(params.productId);
-
-  return (
-    <div className="container mx-auto px-4 py-8">
-      {/* Renderizamos el componente interactivo para detalle de producto */}
-      <ProductDetailClient product={product} />
-    </div>
-  );
+  return <ProductDetailClient product={product} />;
 }
